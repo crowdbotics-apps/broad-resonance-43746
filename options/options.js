e7d8fd0a8e307ce9ba97a3f01cf438f579a849b8
Crowdbotics <team@crowdbotics.com>
2023-10-03 23:25:00 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "broad-resonance-43746",
+  url: "https://broad-resonance-43746.botics.co",
+  api: "https://broad-resonance-43746.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
